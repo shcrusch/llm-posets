ab196eb892493e7b246c11a5a72d0534104d5f7c
ShotaHayashi1996 <hayashi@pajra.c.u-tokyo.ac.jp>
2019-09-22 14:42:16 +0900

    first commit

---
+++ src/python/run.py
@@ -0,0 +1,41 @@
+import tbm
+import sys
+import os
+
+args=sys.argv
+
+X= []
+m= 1000
+n= -1
+
+with open(args[1]) as f:
+    for line in f:
+        xi =line.split()
+        xi_int = [ int(xij)  for xij in xi ]        
+        if m > min(xi_int):
+            m = min(xi_int)
+
+        if n < max(xi_int):
+            n = max(xi_int)
+
+with open(args[1]) as f:
+    for line in f:
+        xi = line.split()
+        xi_int = [ int(xij) - m for xij in xi ]
+        X.append(tuple(xi_int))
+n = n-m+1        
+
+B = tbm.get_B_from(args[2])
+
+S = list(dict.fromkeys(B+X))
+
+tbm = tbm.TBM(n,B,S,X)
+
+if args[3] == "grad":
+    tbm.fit(X,1000, stepsize = float(args[4]) , solver = "grad")
+elif args[3] == "coor":
+    tbm.fit(X,1000, absd = int(args[4]) , solver = "coor")
+elif args[3] == "coor2":
+    tbm.fit(X, 1000, solver = "coor2")
+else:
+    print("Error")
---
+++ src/python/tbm.py
@@ -0,0 +1,408 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+ 
+def get_B_from(filename):
+  # file format:
+  # each line is a frequent itemset with 1-start
+  # ignore the first line that represents "bottom"
+  
+  Bfile = pd.read_csv(filename, header=None, sep=' ',names=[0,1])
+
+  B1=[(),]
+  B2=[]
+  for i in range(len(Bfile)):
+    if pd.isnull(Bfile.at[i,1]): 
+      B1.append((Bfile.loc[i,0]-1,)) 
+
+  for j in range(len(Bfile)):
+    if pd.isnull(Bfile.at[j,1])==0: 
+      B2.append((Bfile.loc[j,0]-1,int(Bfile.loc[j,1]-1)))
+  B1.sort()
+  B2.sort()
+  B=B1+B2
+
+  return B
+
+def includes(phi,x):
+  return set(phi).issubset(x)
+
+class TBM:
+  def __init__ (self, n, B, S, X):
+    
+    self.set_B(B)
+    self.set_S(S)
+    self.n_= n
+    self.set_Phi(self.S_, self.B_)
+    self.set_Ssub(self.S_, self.B_)
+    self.set_Xsub(X)
+  def fit(self, X, n_iter, stepsize=-1,  absd = -1, solver="grad"):
+    """Actual implementation of TBM fitting.
+        Parameters
+        ----------
+        X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s.
+        n_iter: number of iteration
+        method: "grad"  = gradient descent, "coor" coordinate descent
+        Returns
+        -------
+        self : object
+    """
+    self.compute_Phat(X)
+    self.set_etahat(X)
+    if solver == "grad":
+      self.gradient_descent(X, n_iter, stepsize)
+    elif solver == "coor":
+      self.coordinate_descent(X, n_iter, absd)
+    elif solver == "coor2":
+      self.coordinate_descent2(X, n_iter)
+    else:
+      print("Solver Option Error")
+
+    
+    return self 
+  
+  def set_Phi(self, S, B):
+    """
+    Phi_[x]= { phi in B | x includes phi }
+    """
+    self.Phi_ = {}
+    for x in S:
+      Phix = []
+      for phi in B:
+        if includes(phi,x):
+          Phix.append(phi)
+      self.Phi_[x] = Phix
+
+  def set_Ssub(self, S, B):
+    """
+    Ssub [phi]= { x in S | x includes phi } 
+    """
+    self.Ssub_ = {}
+    for phi in B:
+      ssub = []
+      for x in S:
+        if includes(phi,x):
+          ssub.append(x)
+      self.Ssub_[phi] = ssub
+  
+  def set_Xsub(self, X):
+    self.Xsub = list(dict.fromkeys(X))
+    self.invXsub = {}
+    for i in range(len(self.Xsub)):
+      self.invXsub[self.Xsub[i]] = i
+    
+  def compute_Phat(self, X):
+    self.Phat = np.zeros(len(self.Xsub))
+    for xi in X:
+      self.Phat[self.invXsub[xi]] += 1 / len(X)
+    
+
+  def set_etahat(self, X):
+    """
+        
+        Parameters
+        ----------
+        X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s.
+            
+    """
+    self.etahat_ = {}
+    for phi in self.B_:
+      denom = 0.0
+      for i in range(len(X)):
+        denom += 1 if includes(phi, X[i]) else 0
+      self.etahat_[phi] = denom / len(X)
+
+
+  def compute_ll(self, X):
+    """
+      Compute log likelihood
+      Parameters
+      ----------
+      X : array-like, shape = [n_samples, n_features]
+
+      Returns
+      ----------
+      ret :  log-likelihood
+
+    """
+    ret = 0.0
+    for xi in X:
+      ret += self.compute_logP(xi)
+    return ret
+
+  def set_B(self, B):
+    """
+    Generate B_ and invB_
+    Parameters
+    -----
+    B : Set of parameters
+    
+    invB_ : dict type
+               indices are vectors of set_B
+    """
+    self.B_ = B
+    self.invB_ ={}
+    for i in range(len(B)):
+      self.invB_[B[i]] = i
+      
+
+  def set_S(self, S):
+    """
+    Generate S_ and invS_
+    Parameters
+    -----
+    S : Sample space
+    
+    inv_S : dict type
+               indices are vectors of set_S
+    """
+    self.S_ = S
+    self.invS_ ={}
+    for i in range(len(S)):
+      self.invS_[S[i]] = i
+
+  def compute_logP(self, xi):
+    """
+    Computing lopP
+    Parameters
+    -----
+    xi : i th row vector of X
+    
+    Returns
+    -----
+    ret : logP
+    
+    """
+    ret = 0.0;
+    for phi in self.B_ :
+      ret +=  self.get_theta(phi) if includes(phi, xi) else 0    
+    return ret 
+
+  def get_theta(self, phi):
+    """
+    Getting theta
+    Parameters 
+    -----
+    phi : element of set_B
+    
+    Returns
+    -----
+    ret : theta
+    """
+    return self.theta_[self.invB_[phi]]
+  def set_theta(self, phi, value):
+    
+    self.theta_[self.invB_[phi]] = value
+
+    
+  def init_theta(self):
+    
+    self.theta_ = np.zeros(len(self.B_))
+    self.compute_theta_perp()    
+  
+  def compute_theta_perp(self):
+    """
+    Computing theta_perp
+    
+    Returns
+    -----
+    theta_
+    """
+    r = 0.0 
+    for x in self.S_:
+      s = 0.0 # sum theta_phi phi(x) for theta in B minus perp
+      for phi in self.Phi_[x]:
+        if phi: # is not empty
+          s += self.get_theta(phi)  
+      r += np.exp(s)
+    self.theta_[self.invB_[()]] = -np.log(r)
+  
+  def compute_P(self):
+    """
+    Computing P
+    Returns
+    -----
+    P_ : len(P_) = len(S_)
+    """
+    self.P_ = np.zeros(len(self.S_))
+    for x in self.S_:
+      self.P_[self.invS_[x]] = np.exp( self.compute_logP(x) )
+
+  def compute_eta(self):
+    """
+    Computing eta
+    Returns
+    -----
+    eta_ : dict type
+             len(eta_) = len(B_)
+    """
+    self.eta_ = {}
+    for phi in self.B_ :
+      self.eta_[phi] = 0.0
+      for x in self.Ssub_[phi] :
+        self.eta_[phi] += self.P_[self.invS_[x]]
+  
+  def compute_KL(self):
+    """
+    Computing KL_divergence
+    
+    Parameter
+    -----
+    X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s. 
+    -----
+    """
+    ret  = 0.0
+    for x in self.Xsub:
+      ret += self.Phat[self.invXsub[x]] * (np.log(self.Phat[self.invXsub[x]]) - self.compute_logP(x))    
+    return ret
+
+  def compute_squared_gradient(self):
+    ret = 0.0
+    for phi in self.B_:
+#      if phi: # is not empty
+      ret += (self.etahat_[phi] - self.eta_[phi] ) ** 2
+    return ret
+
+  def gradient_descent(self, X, max_epoch, step):  
+    """
+    Actual implementation gradient_descent
+    Parameters 
+    -----
+    X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s. 
+    max_epoch
+    step 
+    """
+    
+    self.init_theta()
+    start = time.time()
+    for epoch in range(max_epoch):
+      self.compute_P()
+      self.compute_eta()
+      kl=self.compute_KL()
+      sg=self.compute_squared_gradient()
+      
+      print(epoch ,":",  "KL divergence:",f'{kl:.8f}' ," time : %4.2f"% (time.time()-start), "Squared Gradient:",f'{sg:.10f}')
+      for phi in self.B_:
+        if phi: # is not empty
+          new_theta_phi = self.get_theta(phi) + step * (self.etahat_[phi] - self.eta_[phi] )
+          self.set_theta(phi, new_theta_phi)
+      self.compute_theta_perp()
+      
+  def coordinate_descent(self, X, max_epoch, absd, step_init=2):
+    """
+    Actual implementation coodinate_descent
+    Parameters 
+    -----
+    X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s.
+    max_epoch
+    -----
+    
+    """
+    u = {}
+    for x in self.S_:
+      u[x] = 1.0
+    Z = len(self.S_)
+    self.theta_ =np.zeros(len(self.B_))
+    start = time.time()
+    for epoch in range(max_epoch):
+      self.set_theta( (), -np.log(Z) )
+      self.compute_P()
+      self.compute_eta()
+      kl = self.compute_KL()
+      sg = self.compute_squared_gradient()
+      print(epoch ,":",  "KL divergence:",f'{kl:.8f}' ," time : %4.2f"% (time.time()-start), "Squared Gradient:",f'{sg:.10f}')
+      index = np.random.RandomState(seed=2019).permutation(range(len(self.B_)-1)) 
+
+      for iter in range(len(self.B_) -1):
+        phi = self.B_[index[iter] +1 ] 
+        etahat_phi = self.etahat_[phi] 
+        eta_phi = 0.0
+        for x in self.Ssub_[phi]:
+            eta_phi += u[x]
+        eta_phi /= Z
+        
+        step_base = step_init 
+        new_eta_phi = eta_phi
+ 
+        while np.abs(etahat_phi - new_eta_phi) > 10 ** (-absd):      
+          step = step_base * ( self.etahat_[phi] - new_eta_phi )
+          u_new, Z_new = self.update_uZ(u, Z, step, phi)
+          
+          eta_phi_step  = 0.0
+          for x in self.Ssub_[phi]:
+              eta_phi_step += u_new[x]
+          eta_phi_step /= Z_new
+
+          if np.abs(etahat_phi - new_eta_phi) >  np.abs(etahat_phi - eta_phi_step ):
+            self.set_theta(phi, self.get_theta(phi) + step )
+            new_eta_phi = eta_phi_step
+            u, Z = u_new, Z_new            
+          else: 
+            step_base *= 0.3
+    
+  def update_uZ(self, u, Z, step, phi):
+    """
+    Updating u,Z
+    Parameters
+    -----
+    u
+    Z
+    step
+    phi
+    
+    """
+    u_new = u.copy()
+    for x in self.Ssub_[phi]:
+        u_new[x] *= np.exp(step)
+    Z_new = Z
+    for x in self.Ssub_[phi]:
+        Z_new += u[x] * (np.exp(step) -1)
+    return u_new, Z_new
+  
+  def coordinate_descent2(self, X, max_epoch):  
+    """
+    Actual implementation gradient_descent
+    Parameters 
+    -----
+    X : array-like, shape (n_samples,)
+            Training vectors, where n_samples is the number of samples and 
+            each element is a tuple of indices of 1s. 
+    max_epoch
+    """
+    self.init_theta()
+    start = time.time()
+    self.compute_P()
+    self.compute_eta()
+    for epoch in range(max_epoch):
+      
+      print(epoch ,":",  "KL divergence:",f'{self.compute_KL():.8f}' ," time : %4.2f"% (time.time()-start),"Squared Gradient:",f'{self.compute_squared_gradient():.10f}')
+      index = np.random.RandomState(seed=2019).permutation(range(len(self.B_)-1)) #exclude the bottom
+      #index = range(len(self.B_)-1)
+      for iter in range(len(self.B_) -1):
+        phi = self.B_[index[iter] +1 ]
+        
+        S, T = self.compute_ST(phi)
+        d = np.log(T / S * self.etahat_[phi] / (1 - self.etahat_[phi]) )
+        new_theta_phi = self.get_theta(phi) + d
+        self.set_theta(phi, new_theta_phi)
+        self.theta_[self.invB_[()]] = -np.log(S * np.exp(d) + T)
+        self.compute_P()
+        self.compute_eta()
+  
+  def compute_ST(self,phi):
+    S = self.eta_[phi] * np.exp(-self.theta_[self.invB_[()]])
+    T = (1 - self.eta_[phi]) * np.exp(-self.theta_[self.invB_[()]])
+    
+    return S,T
+  
