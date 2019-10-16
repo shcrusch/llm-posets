fd172cadaf18bffc114b45a305b90aff3f8d38b3
ShotaHayashi1996 <hayashi@pajra.c.u-tokyo.ac.jp>
2019-10-16 15:39:43 +0900

    added experiments run.sh

---
+++ experiments/run_mushroom.sh
@@ -0,0 +1,7 @@
+basedir=/home/hayashi/workspace/TBM
+srcdir=$basedir/src/python
+datadir=$basedir/dataset
+for step in {1,0.9,0.5,0.1,0.01}; do
+    python $srcdir/run.py $datadir/mushroom.dat $datadir/mushroom.dat_itemsets grad $step > $basedir/experiments/mushroom/grad_$step.txt 2> $basedir/experiments/mushroom/log/grad_$step.log &
+done
+
