37d920e41181793e0d2b4468b906b3316c3edabd
shcrusch <hayashi@pajra.c.u-tokyo.ac.jp>
2021-01-23 14:00:56 +0900

    minor update

--- experiments/run_mushroom.sh
+++ experiments/run_mushroom.sh
@@ -1,5 +1,4 @@
-
-srcdir=$basedir/src/python
+srcdir=../src/python
 
 for step in {0.01,0.1,1,10}; do
     python $srcdir/run.py mushroom grad $step 101 > mushroom/grad_$step.txt 2> mushroom/log/grad_$step.log &
